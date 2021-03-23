a24c0347b828aa420c7105a578474631fcb33702
ogustavoabreu <66218221+ogustavoabreu@users.noreply.github.com>
2021-03-22 21:16:09 -0300

    Melhoria do código do modal

--- app.js
+++ app.js
@@ -65,11 +65,25 @@ function cadastrarDespesa() {
     )
     
     if(despesa.validarDados()){
-        bd.gravar(despesa)
+        //bd.gravar(despesa)
+         
+        document.getElementById('modal_titulo').innerHTML = 'Registro inserido com sucesso'
+        document.getElementById('modal_titulo_div').className = 'modal-header text-success'
+        document.getElementById('modal_conteudo').innerHTML = 'Despesa cadastrada com sucesso'
+        document.getElementById('modal_btn').innerHTML = 'Voltar'
+        document.getElementById('modal_btn').className = 'btn btn-success'       
+        
         //diálogo de sucesso
         $('#modalRegistraDespesa').modal('show')
     } else{
-        //diálogo de erro
+
+        document.getElementById('modal_titulo').innerHTML = 'Erro na inclusão do registro'
+        document.getElementById('modal_titulo_div').className = 'modal-header text-danger'
+        document.getElementById('modal_conteudo').innerHTML = 'Erro na gravação, verifique se todos os campos foram preenchidos corretamente'
+        document.getElementById('modal_btn').innerHTML = 'Voltar e corrigir'
+        document.getElementById('modal_btn').className = 'btn btn-danger'
+
+         //diálogo de erro
         $('#modalRegistraDespesa').modal('show')
     }
     
