bfe522515c17927d3d6d9ad00dd6cac1ba39dd16
ogustavoabreu <66218221+ogustavoabreu@users.noreply.github.com>
2021-03-24 20:31:56 -0300

    Ajuste de validação de dados

--- app.js
+++ app.js
@@ -37,9 +37,33 @@ class Bd {
     gravar(d) {
         let id = this.getProximoId()
 
-        localStorage.setItem('id', JSON.stringify(d))
+        localStorage.setItem(id, JSON.stringify(d))
 
         localStorage.setItem('id', id)
+    }
+
+    recuperarTodosRegistros() {
+
+        //array de despesas
+        let despesas = Array()
+
+        let id = localStorage.getItem('id')
+
+        //recuperar todas as despesas cadastradas em localStorage
+        for(let i = 1; i <= id; i++){
+
+            //recuperar a despesa
+            let despesa = JSON.parse(localStorage.getItem(i))
+
+            //existe a possibilidade de haver índices que foram removidos
+            if(despesa === null) {
+                continue 
+            }
+
+            despesas.push(despesa)
+        }
+
+        return despesas
     }
 }
 
@@ -64,8 +88,8 @@ function cadastrarDespesa() {
         valor.value
     )
     
-    if(despesa.validarDados()){
-        //bd.gravar(despesa)
+    if(despesa.validarDados()) {
+        bd.gravar(despesa)
          
         document.getElementById('modal_titulo').innerHTML = 'Registro inserido com sucesso'
         document.getElementById('modal_titulo_div').className = 'modal-header text-success'
@@ -75,6 +99,13 @@ function cadastrarDespesa() {
         
         //diálogo de sucesso
         $('#modalRegistraDespesa').modal('show')
+
+        ano.value = ''
+        mes.value = ''
+        dia.value = ''
+        tipo.value = ''
+        descricao.value = ''
+        valor.value = ''
     } else{
 
         document.getElementById('modal_titulo').innerHTML = 'Erro na inclusão do registro'
@@ -86,6 +117,48 @@ function cadastrarDespesa() {
          //diálogo de erro
         $('#modalRegistraDespesa').modal('show')
     }
-    
 }
+
+function carregarListaDespesas() {
+
+    let despesas = Array()
+
+    despesas = bd.recuperarTodosRegistros()
+
+    //seleciona o elemento tbody da tabela
+    let listaDespesas = document.getElementById('listaDespesas')
+
+    //percorrer o array despesas listando cada despesa de forma de dinâmica 
+    despesas.forEach(function(d) {
+
+        //console.log(d)
+
+        //criação da linha (tr)
+        let linha = listaDespesas.insertRow()
+
+        //criação da coluna (td)
+        linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}` 
+        linha.insertCell(1).innerHTML = d.tipo
+
+        //ajuste do tipo
+        switch(d.tipo){
+            case '1': d.tipo = 'Alimentação'
+                break
+            case '2': d.tipo = 'Educação'
+                break
+            case '3': d.tipo = 'Lazer'
+                break
+            case '4': d.tipo = 'Saúde'
+                break
+            case '5': d.tipo = 'Transporte'
+                break
+        }
+
+
+        linha.insertCell(2).innerHTML = d.descricao
+        linha.insertCell(3).innerHTML = d.valor
+
+    })
+
+}   
 
